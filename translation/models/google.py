41561281c891a35cac78f7c487e2bd05d63f7b79
LittleCoder <i7meavnktqegm1b@qq.com>
2016-06-20 12:43:54 +0800

    Add google translation

--- translation/models/google.py
+++ translation/models/google.py
@@ -1,2 +1,21 @@
+import requests
+
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:5.0) Gecko/20100101 Firefox/5.0', }
+url = 'http://translate.google.cn/translate_a/t'
+params = {
+    'client': 'p',
+    'ie': 'UTF-8',
+    'oe': 'UTF-8',
+    'tl': None,
+    'sl': None,
+    'text': None, }
+
 def google(text, src, dst, proxies):
-    return 'Result'
+    params['text'] = text
+    params['tl'] = dst
+    params['sl'] = src
+    return requests.post(url, params, headers = headers, proxies = proxies).json()[0]
+
+if __name__ == '__main__':
+    print(google('Test', 'auto', 'zh-CN', {'http': 'http://127.0.0.1:22397'}))
